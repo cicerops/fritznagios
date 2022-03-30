4b644923903f0abccdabfa05793515375547db5f
Andreas Motl <andreas.motl@cicerops.de>
2022-03-30 23:42:51 +0200

    Code formatting, project metadata and documentation

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8; -*-
-
 import os
 
 from setuptools import setup
@@ -18,12 +16,12 @@ setup(
     url="https://github.com/cicerops/fritznagios",
     author="Jan Hoffmann",
     author_email="jan.hoffmann@bergamsee.de",
-    description="Monitoring sensor for Fritzboxes",
+    description="Nagios/Icinga monitoring check program for FRITZ!Box devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
     platforms=["any"],
     license="MIT",
-    keywords="AVM, FRITZ!Box, fritzbox, fritz, Nagios, Icinga",
+    keywords="FRITZ!Box, AVM, fritzbox, fritz, Nagios, Icinga, monitoring, TR-064, UPnP",
     py_modules=["fritznagios"],
     entry_points={"console_scripts": ["fritznagios = fritznagios:main"]},
     python_requires=">=3.4",
@@ -50,6 +48,9 @@ setup(
         "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
         "Topic :: Software Development :: Libraries",
         "Topic :: System :: Archiving",
+        "Topic :: System :: Logging",
+        "Topic :: System :: Monitoring",
+        "Topic :: System :: Systems Administration",
         "Topic :: Text Processing",
         "Topic :: Utilities",
     ],
